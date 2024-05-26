245a4b3462f85c2e28bb38022a2eb98f35e1989c
Daniel Gallah <gallahdaniel666@gmail.com>
2024-05-26 08:33:51 +0400

    Make a head program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод строк с клавиатуры
+        Console.WriteLine("Введите строки через запятую:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+
+        // Создание списка для выходного массива
+        List<string> outputList = new List<string>();
+
+        // Проход по входному массиву и добавление строк длиной <= 3
+        foreach (string str in inputArray)
+        {
+            if (str.Trim().Length <= 3)
+            {
+                outputList.Add(str.Trim());
+            }
+        }
+
+        // Вывод выходного массива
+        Console.WriteLine("Output Array: " + string.Join(", ", outputList));
+    }
+}
